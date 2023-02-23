81a0000114b122d7051aca12f9bfac2acc7283fe
Do Van Thanh <dvthanh277@aiaracorp.com>
2023-02-23 17:17:23 +0700

    Test print

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-function printHTML() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://192.168.1.87:9100');
-
-    // Thiết lập tiêu đề của yêu cầu in
-    xhr.setRequestHeader('Content-Type', 'application/raw');
-
-    // Chuyển đổi nội dung HTML sang định dạng RAW
-    var rawContent = '<RAW>' + 'Hello' + '</RAW>';
-
-    // Gửi yêu cầu in tới máy in
-    xhr.send(rawContent);
-}
+const printButton = document.getElementById('printButton');
+printButton.addEventListener('click', function() {
+  window.print();
+});
