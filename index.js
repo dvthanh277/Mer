91029d094b37c9d2fc7ab493b8bbd7e1552c6812
Do Van Thanh <dvthanh277@aiaracorp.com>
2023-03-14 10:10:46 +0700

    update topping

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import { formatNumber } from "./js/common/numbers.utils.js";
 
 const menu_items = document.querySelector("#menu_items");
+const topping_items = document.querySelector("#topping_items");
 const bill_items = document.querySelector("#bill_items");
 const search_input = document.querySelector("#search_input");
 const total_quantity_element = document.querySelector("#total_quantity");
 const total_money_element = document.querySelector("#total_money");
 var products;
-
+var topping;
 var bills = [];
+var topping_adds = [];
 window.onload = async function () {
   getProducts();
   search_input.focus();
@@ -18,16 +20,14 @@ window.onload = async function () {
   document.addEventListener("keydown", function (event) {
     switch (event.code) {
       case "F1":
-        console.log("You just pressed F1!");
         printPage();
         break;
       case "F2":
-        console.log("You just pressed F2!");
+        printBill();
         break;
     }
   });
 };
-
 window.handleAddItem = (id, size) => {
   const index = bills.findIndex((element) => element.invoiceId === id + size);
   if (index !== -1) {
@@ -44,24 +44,64 @@ window.handleAddItem = (id, size) => {
       invoiceId: product[0].id + size,
       size: size,
       price: price,
+      idProduct: product[0].id,
       quantity: 1,
+      topping: [],
       total: price,
     };
-    console.log(item);
     bills.push(item);
   }
-
   renderBill(bills);
+};
+var idTemp, sizeTemp;
+window.openToppingModal = (id, size) => {
+  idTemp = id;
+  sizeTemp = size;
+  $("#toppingModal").modal("show");
+};
+window.handleAddItemTopping = () => {
+  const product = returnItemInfo(idTemp);
+  let price = product[0].sizes.filter(
+    (sizeElement) => sizeElement.name === sizeTemp
+  )[0].price;
+  let price_total = price;
+  topping_adds.map((element) => {
+    price_total += element.total;
+  });
+  let item = {
+    name: product[0].name,
+    image: product[0].image,
+    invoiceId:
+      product[0].id + sizeTemp + "TP" + Math.floor(Math.random() * 1000),
+    size: sizeTemp,
+    price: price,
+    quantity: 1,
+    idProduct: product[0].id,
+    total: price_total,
+    topping: topping_adds,
+  };
+  bills.push(item);
+  $("#toppingModal").modal("hide");
+  topping_adds = [];
+  renderBill(bills);
+  console.log(bills);
 };
 window.returnItemInfo = (id) => {
   const copy = [...products];
   let temp = copy.filter((element) => element.id == id);
   return temp;
 };
-
+window.returnToppingItem = (id) => {
+  const copy = [...topping];
+  let temp = copy.filter((element) => element.id == id);
+  return temp;
+};
 const getProducts = async () => {
   products = await API.getData("product");
+  topping = await API.getData("topping");
+  console.log(topping);
   renderProduct(products);
+  renderTopping(topping);
 };
 const renderProduct = (data) => {
   if (data) {
@@ -91,49 +131,22 @@ const renderProduct = (data) => {
                   return `<div class="d-flex align-items-center justify-content-between col-12 mb-2">
                 <span class="text-primary fw-bolder me-2">${formatNumber(
                   size.price
-                )} đ</span
-                >
-                <div onclick="handleAddItem('${element.id}','${size.name}')">
-                <span class="fw-bolder mr-2 text-left">${size.name}</span>
-                <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  class="circle-1"
-                  width="24"
-                  height="24"
-                  rx="12"
-                  fill="currentColor"
-                />
-                <rect
-                  class="circle-2"
-                  x="11.168"
-                  y="7"
-                  width="1.66667"
-                  height="10"
-                  rx="0.833333"
-                  fill="currentColor"
-                />
-                <rect
-                  class="circle-3"
-                  x="7"
-                  y="12.834"
-                  width="1.66666"
-                  height="10"
-                  rx="0.833332"
-                  transform="rotate(-90 7 12.834)"
-                  fill="currentColor"
-                />
-              </svg> </div>
+                )} đ</span>
+                <div class="d-flex align-items-center justify-content-between">
+            
+              <button class="btn btn-success btn-sm rounded-pill fw-bolder" onclick="handleAddItem('${
+                element.id
+              }','${size.name}')" style="min-width:47px">
+            ${size.name}</button>
+
+              <button class="btn btn-warning btn-xs rounded-pill ms-3" onclick="openToppingModal('${
+                element.id
+              }','${size.name}')">
+              Topping</button>
+              </div>
               </div>`;
                 })
                 .join("")}
-              
-              
             </div>
           </div>
         </div>
@@ -141,6 +154,57 @@ const renderProduct = (data) => {
       })
       .join("");
   }
+};
+
+const renderTopping = (data) => {
+  if (data) {
+    topping_items.innerHTML = data
+      .map((element) => {
+        return `<div class="col-4 mb-3" onclick="handleAddTopping('${
+          element.id
+        }')" >
+        <div
+          class="card card-white dish-card profile-img mb-0 index"
+        >
+          <div class="p-3">
+            <h6 class="heading-title fw-bolder mt-2 mb-0">
+              ${element.name}
+            </h6>
+            <div class="d-flex mt-1 flex-wrap">
+            <div class="d-flex align-items-center justify-content-between col-12 mb-2">
+            <span class="text-primary fw-bolder me-2">${formatNumber(
+              element.price
+            )} đ</span>
+            </div>
+            </div>
+
+          </div>
+        </div>
+      </div>`;
+      })
+      .join("");
+  }
+};
+
+window.handleAddTopping = (id_topping) => {
+  const index = topping_adds.findIndex((element) => element.id === id_topping);
+  if (index !== -1) {
+    topping_adds[index].quantity += 1;
+    topping_adds[index].total =
+      topping_adds[index].quantity * topping_adds[index].price;
+  } else {
+    const topping = returnToppingItem(id_topping);
+    let item = {
+      id: topping[0].id,
+      name: topping[0].name,
+      price: topping[0].price,
+      quantity: 1,
+      total: topping[0].price,
+    };
+    topping_adds.push(item);
+  }
+  console.log(topping_adds);
+  // renderToppingAdd(topping_adds);
 };
 var total_money = 0;
 var total_quantity = 0;
@@ -178,7 +242,7 @@ const renderBill = (data) => {
               <button type="button" class="btn btn-primary btn-xs" onclick="handleIncrease('${
                 element.invoiceId
               }')"> + </button>
-
+         
             </span>
           </div>
         </div>
@@ -202,9 +266,49 @@ const renderBill = (data) => {
               />
             </svg>
           </span>
-          <p class="mb-0 text-dark">${formatNumber(element.total)} đ</p>
+          <p class="mb-0 text-dark">${
+            element.topping.length > 0
+              ? `<span class="old-price">${formatNumber(
+                  element.price
+                )}đ</span>${formatNumber(element.total)} đ`
+              : `${formatNumber(element.total)}đ`
+          }</p>
         </div>
       </div>
+      ${element.topping
+        .map((topping) => {
+          return `
+        <div class="d-flex align-items-center justify-content-between profile-img4">
+        <div class="d-flex align-items-center profile-content">
+          <div>
+            <h6 class="mb-1 heading-title fw-bolder">
+              ${topping.name}
+            </h6>
+            <span class="d-flex align-items-center">
+              <button type="button" class="btn btn-primary btn-xs" onclick="handleDecreaseTopping('${
+                topping.id
+              }')"> - </button>
+              <small class="text-dark fw-bold ms-3 me-3">x ${
+                topping.quantity
+              }</small>
+              <button type="button" class="btn btn-primary btn-xs" onclick="handleIncreaseTopping('${
+                topping.id
+              }')"> + </button>
+            </span>
+          </div>
+        </div>
+        <div class="me-4 text-end">
+          <span class="mb-1" onclick="handleDeleteTopping('${topping.id}')">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path opacity="0.4" d="M19.6449 9.48924C19.6449 9.55724 19.112 16.298 18.8076 19.1349C18.6169 20.8758 17.4946 21.9318 15.8111 21.9618C14.5176 21.9908 13.2514 22.0008 12.0055 22.0008C10.6829 22.0008 9.38936 21.9908 8.1338 21.9618C6.50672 21.9228 5.38342 20.8458 5.20253 19.1349C4.88936 16.288 4.36613 9.55724 4.35641 9.48924C4.34668 9.28425 4.41281 9.08925 4.54703 8.93126C4.67929 8.78526 4.86991 8.69727 5.07026 8.69727H18.9408C19.1402 8.69727 19.3211 8.78526 19.464 8.93126C19.5973 9.08925 19.6644 9.28425 19.6449 9.48924" fill="#E60A0A"></path>
+              <path d="M21 5.97686C21 5.56588 20.6761 5.24389 20.2871 5.24389H17.3714C16.7781 5.24389 16.2627 4.8219 16.1304 4.22692L15.967 3.49795C15.7385 2.61698 14.9498 2 14.0647 2H9.93624C9.0415 2 8.26054 2.61698 8.02323 3.54595L7.87054 4.22792C7.7373 4.8219 7.22185 5.24389 6.62957 5.24389H3.71385C3.32386 5.24389 3 5.56588 3 5.97686V6.35685C3 6.75783 3.32386 7.08982 3.71385 7.08982H20.2871C20.6761 7.08982 21 6.75783 21 6.35685V5.97686Z" fill="#E60A0A"></path>
+            </svg>
+          </span>
+          <p class="mb-0 text-dark">${formatNumber(topping.total)} đ</p>
+        </div>
+      </div>`;
+        })
+        .join("")}
     </div>`;
       })
       .join("");
@@ -309,7 +413,23 @@ window.printPage = () => {
       <span class="date">${date}</span><span class="time">${time}</span>
     </p>
     <p class="print-title">${element.name} (${element.size})</p>
-    <p class="print-price">${formatNumber(element.price)} đ</p>
+    <p class="print-topping">
+      ${element.topping
+        .map((topping) => {
+          return `<span>${topping.name} x${topping.quantity} - ${formatNumber(
+            topping.total
+          )}</span>`;
+        })
+        .join("")}
+    </p>
+    <p class="print-price">${
+      element.topping.length > 0
+        ? `<span class="old-price">${formatNumber(
+            element.price
+          )}đ</span>${formatNumber(element.total)}đ`
+        : `${formatNumber(element.total)}đ`
+    }</p>
+    
   </div>`;
     })
     .join("");
@@ -321,11 +441,11 @@ window.printPage = () => {
   printWindow.document.write("</head><body><div id='printContent'>");
   printWindow.document.write(printContent.innerHTML); // in nội dung muốn in vào trang in mới
   printWindow.document.write("</div></body></html>");
-  printWindow.document.close(); // đóng trang in mới
-  setTimeout(function () {
-    printWindow.print(); // thực hiện lệnh in trang in mới
-    printWindow.close(); // đóng trang in mới sau khi in xong
-  }, 500);
+  // printWindow.document.close(); // đóng trang in mới
+  // setTimeout(function () {
+  //   printWindow.print(); // thực hiện lệnh in trang in mới
+  //   printWindow.close(); // đóng trang in mới sau khi in xong
+  // }, 500);
 };
 window.printBill = () => {
   if (!bills || bills.length <= 0) return;
@@ -371,11 +491,41 @@ window.printBill = () => {
               .map((element) => {
                 console.log(element);
                 total += element.total;
-                return `<tr>
-                <td>${element.name} (${element.size})</td>
-                <td>x${element.quantity}</td>
-                <td>${formatNumber(element.total)}</td>
-              </tr>`;
+                if (element.topping.length > 0) {
+                  return `<tr>
+                  <td><span class="name">${element.name} (${
+                    element.size
+                  })</span>
+                    ${element.topping
+                      .map((topping) => {
+                        return `<span class="topping">${topping.name}</span>`;
+                      })
+                      .join("")}
+                  </td>
+                  <td><span class="quantity">x${element.quantity}</span>
+                  ${element.topping
+                    .map((topping) => {
+                      return `<span class="quantity">x${topping.quantity}</span>`;
+                    })
+                    .join("")}
+                   </td>
+                  <td><span class="total">${formatNumber(element.price)}</span>
+                  ${element.topping
+                    .map((topping) => {
+                      return `<span class="total">${formatNumber(
+                        topping.total
+                      )}</span>`;
+                    })
+                    .join("")}
+                  </td>
+                </tr>`;
+                } else {
+                  return `<tr>
+                  <td>${element.name} (${element.size})</td>
+                  <td>x${element.quantity}</td>
+                  <td>${formatNumber(element.total)}</td>
+                </tr>`;
+                }
               })
               .join("")}
             </tbody>
@@ -398,9 +548,9 @@ window.printBill = () => {
   printWindow.document.write("</head><body><div id='printBill'>");
   printWindow.document.write(printBill.innerHTML); // in nội dung muốn in vào trang in mới
   printWindow.document.write("</div></body></html>");
-  printWindow.document.close(); // đóng trang in mới
-  setTimeout(function () {
-    printWindow.print(); // thực hiện lệnh in trang in mới
-    printWindow.close(); // đóng trang in mới sau khi in xong
-  }, 500);
+  // printWindow.document.close(); // đóng trang in mới
+  // setTimeout(function () {
+  //   printWindow.print(); // thực hiện lệnh in trang in mới
+  //   printWindow.close(); // đóng trang in mới sau khi in xong
+  // }, 500);
 };
