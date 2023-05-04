e2b1744556b14bbe607a15800295ffbf4d9b3931
Do Thanh <dvthanh277@aiaracorp.com>
2023-05-04 17:41:20 +0700

    Updated

--- print.js
+++ print.js
@@ -2,12 +2,20 @@ function printDiv() {
   // Get the div element you want to print
   var divToPrint = document.getElementById("printBill");
 
-  // Create a print job
-  var printJob = {
-    printer: "InHoaDon",
-    data: divToPrint.innerHTML,
-  };
 
-  // Send the print job to QZ Tray
-  qz.print(printJob);
+  qz.websocket.connect().then(function () {
+    console.log(qz.printers);
+    return qz.printers.find("InHoaDon");              // Pass the printer name into the next Promise
+  }).then(function (printer) {
+    var config = qz.configs.create(printer);       // Create a default config for the found printer
+    var data = [{
+      type: 'pixel',
+      format: 'html',
+      flavor: 'plain', // or 'plain' if the data is raw HTML
+      data: divToPrint.innerHTML
+    }];
+    return qz.print(config, data);
+  }).catch(function (e) { console.error(e); });
+
 }
+printDiv()
